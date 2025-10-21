d0d8185fcde0223aa1a35b0963392d061bed14c9
renearduo-gif <rene.arduo@gmail.com>
2025-10-21 08:38:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask API is running!"
-
-if __name__ == '__main__':
-    app.run()
-
+    return "Hello, world!"
