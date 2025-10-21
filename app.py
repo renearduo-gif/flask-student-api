0d265d66c3df8a4d45c6b52caabe08c68528e93a
renearduo-gif <rene.arduo@gmail.com>
2025-10-21 09:23:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, world!"
+    return "Welcome to my Flask API!"
+
+@app.route('/student')
+def get_student():
+    return jsonify({
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    })
